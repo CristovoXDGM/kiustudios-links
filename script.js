4fb052095a2ec5a98cb37e20aab525d591cfc8d4
Cristovão Farias <cristovoxdgm@gmail.com>
2021-10-14 11:48:14 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function loadLinks() {
+  var github = document.getElementById("github");
+  var youtube = document.getElementById("youtube");
+  var linkedin = document.getElementById("linkedin");
+  var instagram = document.getElementById("instagram");
+  var twitter = document.getElementById("twitter");
+  var curso = document.getElementById("curso");
+  var meusApps = document.getElementById("meusApps");
+  var faleConosco = document.getElementById("faleconosco");
+
+  curso.href = "https://go.hotmart.com/L50348126U";
+  meusApps.href = "https://play.google.com/store/apps/developer?id=KiuStudios";
+  faleConosco.href = "mailto:kiu.studios@gmail.com";
+  github.href = "https://github.com/CristovoXDGM";
+  youtube.href = "https://www.youtube.com/channel/UCUdmXvjS1pBarFUD5cUpeBQ";
+  linkedin.href =
+    "https://www.linkedin.com/in/cristov%C3%A3o-farias-643bba111/";
+  instagram.href = "https://www.instagram.com/kiustudios/";
+  twitter.href = "https://twitter.com/KiuStudios";
+}
